c6be89806ee410e4da151004a1b21e71100fdc4a
shiquanqiang <2817465796@qq.com>
2020-11-15 22:07:27 +0800

    home界面开发，轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,35 @@
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
+    <home-swiper :banner="banner"/>
+    <recommend-view :recommend="recommend"/>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import homeSwiper from "./childComps/homeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import {getMutiData} from 'network/home'
 
   export default {
     name: "Home",
     components:{
-      NavBar
+      NavBar,
+      homeSwiper,
+      RecommendView
+    },
+    data(){
+      return{
+        banner: [],
+        recommend: []
+      }
+    },
+    created() {
+      getMutiData().then(res=>{
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
     }
   }
 </script>
