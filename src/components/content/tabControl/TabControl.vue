a9be69371e20d779ad17664655f0557c69ecf9f2
shiquanqiang <2817465796@qq.com>
2020-11-17 22:34:30 +0800

    tabControl 开发

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="tab-control">
+      <div v-for="(item,index) in tabData" class="tab-control-item"
+           :class="{active:index===currentIndex}" @click="tabClick(index)">
+        <span>{{item}}</span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "",
+      data() {
+          return{
+            currentIndex: 0,
+          }
+      },
+      props:{
+        tabData:{
+            type: Array,
+            default(){
+              return [];
+            }
+          }
+      },
+      methods:{
+        tabClick(index) {
+          this.currentIndex = index;
+        }
+      },
+    }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    line-height: 44px;
+    height: 44px;
+    font-size: 14px;
+    background-color: #fff;
+  }
+  .tab-control-item{
+    flex: 1;
+  }
+  .active{
+    color: var(--color-tint);
+  }
+  .active span{
+    border-bottom: 2px solid #ff5777;
+  }
+  .tab-control-item span{
+    padding: 5px;
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,21 +5,89 @@
     </nav-bar>
     <home-swiper :banner="banner"/>
     <recommend-view :recommend="recommend"/>
+    <feature-view/>
+    <tab-control class="tab-control" :tabData="['流行','新款','精品']"/>
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+
   import homeSwiper from "./childComps/homeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+
+
   import {getMutiData} from 'network/home'
 
   export default {
     name: "Home",
     components:{
+      FeatureView,
       NavBar,
       homeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     data(){
       return{
@@ -37,8 +105,22 @@
 </script>
 
 <style scoped>
-.home-nav{
+.home-nav {
   color: #f6f6f6;
-  background-color:var(--color-tint)
+  background-color: var(--color-tint);
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+
 }
+  #home{
+    padding-top: 44px;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
 </style>
